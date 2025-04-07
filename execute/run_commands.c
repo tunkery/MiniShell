b8616f1f49ab06b81b646db807b4dded1d6efbf1
tunkery <tunkery@gmail.com>
2025-04-07 15:04:53 +0200

    dollar sign + PWD/USER/PATH etc gives the correct output

--- execute/run_commands.c
+++ execute/run_commands.c
@@ -6,7 +6,7 @@
 /*   By: bolcay <bolcay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 13:26:21 by bolcay            #+#    #+#             */
-/*   Updated: 2025/04/02 17:49:30 by bolcay           ###   ########.fr       */
+/*   Updated: 2025/04/07 15:03:08 by bolcay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,25 +48,64 @@ static void	run_without_path(char **args, t_env *env, int out_fd, char *exe)
 		wait_for_child(pid, env);
 }
 
-static void	run_with_path(char **args, t_env *env, int out_fd)
+// static void	run_with_path(char **args, t_env *env, int out_fd)
+// {
+// 	pid_t	pid;
+
+// 	if (access(args[0], X_OK) == 0)
+// 	{
+// 		pid = fork();
+// 		if (pid == 0)
+// 		{
+// 			if (out_fd != STDOUT_FILENO)
+// 			{
+// 				dup2(out_fd, STDOUT_FILENO);
+// 				close(out_fd);
+// 			}
+// 			if (execve(args[0], args, env->envp) == -1)
+// 			{
+// 				perror("execvp Failed");
+// 				exit(127);
+// 			}
+// 		}
+// 		else if (pid < 0)
+// 			perror("fork Failed");
+// 		else
+// 			wait_for_child(pid, env);
+// 	}
+// 	else
+// 		printf("minishell: %s: command not found.\n", args[0]);
+// }
+
+static void	run_with_path(char *str, char **args, t_env *env, int out_fd)
 {
 	pid_t	pid;
+	struct stat info;
 
-	if (access(args[0], X_OK) == 0)
+	(void)out_fd;
+	if (stat(str, &info) != 0)
+	{
+		printf("minishell: %s: No such file or directory\n", str);
+		return ;
+	}
+	else if (S_ISDIR(info.st_mode))
+	{
+		printf("minishell: %s: is a directory\n", str);
+		return ;
+	}
+	else if (access(str, X_OK) != 0)
+	{
+		printf("minishell: %s: Permission denied\n", str);
+		return ;
+	}
+	else if (access(str, X_OK) == 0)
 	{
 		pid = fork();
 		if (pid == 0)
 		{
-			if (out_fd != STDOUT_FILENO)
-			{
-				dup2(out_fd, STDOUT_FILENO);
-				close(out_fd);
-			}
-			if (execve(args[0], args, env->envp) == -1)
-			{
-				perror("execvp Failed");
-				exit(127);
-			}
+			execve(str, args, env->envp);
+			printf("minishell: %s: Permission denied\n", str);
+			exit(127);
 		}
 		else if (pid < 0)
 			perror("fork Failed");
@@ -74,78 +113,61 @@ static void	run_with_path(char **args, t_env *env, int out_fd)
 			wait_for_child(pid, env);
 	}
 	else
-		printf("minishell: %s: command not found.\n", args[0]);
+		printf("minishell: %s: command not found.\n", str);
 }
 
-static char	*get_path(char *str, t_env *env)
-{
-	int		i;
-	int		j;
-	int		size;
-	char	*key;
-	char	*temp;
-	char	*temp2;
-	char	*final;
+// static char	*get_path(char *str, t_env *env)
+// {
+// 	int		i;
+// 	int		j;
+// 	int		size;
+// 	char	*key;
+// 	char	*temp;
+// 	char	*temp2;
+// 	char	*final;
 
-	i = 1;
-	j = 0;
-	key = ft_substr(str, 1, key_size(str));
-	size = ft_strlen(key);
-	while (env->envp[j] && ft_strncmp(env->envp[j], key, size) != 0)
-		j++;
-	if (!env->envp[j])
-		return (NULL);
-	temp = ft_substr(env->envp[j], size + 1, ft_strlen(env->envp[j]) - size);
-	temp2 = ft_substr(str, size + 1, ft_strlen(str) - size - 1);
-	final = ft_strjoin(temp, temp2);
-	free(temp);
-	free(temp2);
-	return (final);
-}
+// 	i = 1;
+// 	j = 0;
+// 	key = ft_substr(str, 1, key_size(str));
+// 	size = ft_strlen(key);
+// 	while (env->envp[j] && ft_strncmp(env->envp[j], key, size) != 0)
+// 		j++;
+// 	if (!env->envp[j])
+// 		return (NULL);
+// 	temp = ft_substr(env->envp[j], size + 1, ft_strlen(env->envp[j]) - size);
+// 	temp2 = ft_substr(str, size + 1, ft_strlen(str) - size - 1);
+// 	final = ft_strjoin(temp, temp2);
+// 	free(temp);
+// 	free(temp2);
+// 	return (final);
+// }
 
 void	exec_command(char **args, t_env *env, int out_fd)
 {
 	char	*exec_path;
 	char	*path;
-	char	*temp;
+	char	*final;
 
 	if (!args || !args[0])
 		return ;
+	final = args[0];
 	path = find_path(env);
-	if (ft_strchr(args[0], '/')) // if, yani eger, biri path ile yazmissa komutu burasi kontrol edip calistiriyo
+	if (ft_strchr(final, '/')) // if, yani eger, biri path ile yazmissa komutu burasi kontrol edip calistiriyo
 	{
 		exec_path = NULL;
-		run_with_path(args, env, out_fd);
+		run_with_path(final, args, env, out_fd);
 	}
 	else
 		// eger pathsiz yazilmissa command burasi isi eline aliyo ve pathi bulup calistiriyo
 	{
-		exec_path = find_exec(args[0], path, 0, 5);
+		exec_path = find_exec(final, path, 0, 5);
 		if (!exec_path)
 		{
-			if (args[0] && args[0][0] == '$')
-			{
-				temp = get_path(args[0], env);
-				if (ft_strchr(args[0], '=') == 0)
-				{
-					printf("minishell: %s: is a directory\n", temp);
-					env->exit_code = 126;
-				}
-				else
-				{
-					printf("minishell: %s: No such file or directory\n", temp);
-					env->exit_code = 127;
-				}
-				DEBUG_PRINT(GRN "exit status: %d\n" RESET, env->exit_code);
-				free(temp);
-				return ;
-			}
 			printf("minishell: %s: command not found.\n", args[0]);
 			env->exit_code = 127;
-			DEBUG_PRINT(GRN "exit status: %d\n" RESET, env->exit_code);
-			return ;
 		}
-		run_without_path(args, env, out_fd, exec_path);
+		else
+			run_without_path(args, env, out_fd, exec_path);
 	}
 	DEBUG_PRINT(GRN "exit status: %d\n" RESET, env->exit_code);
 	if (exec_path)
